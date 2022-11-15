d5365a7c16c4b764f82d36b627e6dcf2a8bd5ada
Twice <njabulo.magagula@ribccs.com>
2022-11-15 12:23:44 +0200

    + created squares and renderSquare vals

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+class Game extends React.Component 
+{
+	render()
+	{
+		return (
+			<div className="game">
+				<div className="game-board">
+					<Board />
+				</div>
+			</div>
+		)
+	}
+}
+
+class Board extends React.Component 
+{
+	renderSquare(i) 
+	{
+		return (
+			<Square value={i} />
+		)
+	}
+
+	render()
+	{
+		const status = "Next player: X";
+		return (
+			<div>
+				<div className="status">{status}</div>
+				<div className="board-row">
+					{this.renderSquare(0)}
+					{this.renderSquare(1)}
+					{this.renderSquare(2)}
+				</div>
+				<div className="board-row">
+					{this.renderSquare(3)}
+					{this.renderSquare(4)}
+					{this.renderSquare(5)}
+				</div>
+				<div className="board-row">
+					{this.renderSquare(6)}
+					{this.renderSquare(7)}
+					{this.renderSquare(8)}
+				</div>
+			</div>
+		)
+	}
+}
+
+class Square extends React.Component 
+{
+	constructor(props)
+	{
+		super(props);
+		this.state = {
+			value: null
+		};
+	}
+
+	render() {
+		return (
+			<button className="square" 
+					onClick={() => { this.setState({value: 'X'})}}>
+				{this.props.value}
+			</button>
+		);
+	}
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+		<Game />
     </div>
   );
 }
